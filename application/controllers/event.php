01ec2b96c3c892065e6e87cd047673e0114717f5
flashlizi <flashlizi@gmail.com>
2012-07-25 13:59:43 +0800

    event update

--- application/controllers/event.php
+++ application/controllers/event.php
@@ -9,33 +9,47 @@ class Event extends CI_Controller
 		$status = "";
 		$msg = "";
 
-		if(empty($_POST["name"]) || empty($_POST["owner_id"]) || empty($_POST["start_date"]))
+		echo print_r($_POST);
+
+		if(empty($_POST["owner_id"]) || empty($_POST["password"]) || empty($_POST["event_name"]) || empty($_POST["start_date"]))
 		{
 			$status = "error";
 			$msg = "Post data error.";
 		}else{
-			$data = array(
-				'name' => $this->input->post('name'), 
-				'owner_id' => $this->input->post('owner_id'),
-				'start_date' => $this->input->post('start_date'),
-				'end_date' => $this->input->post('end_date'),
-				'desc' => $this->input->post('desc'),
-				'detail' => $this->input->post('detail'),
-				'type' => $this->input->post('type'),
-				'venue' => $this->input->post('venue'),
-				'address' => $this->input->post('address'),
-				'logo' => $this->input->post('logo'),
-				'ticket_total' => $this->input->post('ticket_total')
-			);
-			try {
-				$this->db->insert('event', $data);
-				$eventid = $this->db->insert_id();
-				$status = "success";
-				$msg = array('event_id' => $eventid);
-			} catch (Exception $e) {
+			$user_id = $this->input->post('owner_id');
+			$password = $this->input->post('password');
+			$query = $this->db->query('SELECT * FROM user WHERE user_id = '.$this->db->escape($user_id).' AND password = '.$this->db->escape($password).'');
+			if($query->num_rows() == 0)
+			{
 				$status = "error";
-				$msg = "Database error.";
-			}
+				$msg = "Invalid user.";
+			}else
+			{
+				$data = array(
+					'owner_id' => $this->input->post('owner_id'),
+					'name' => $this->input->post('event_name'), 
+					'start_date' => $this->input->post('start_date'),
+					'end_date' => $this->input->post('end_date'),
+					'desc' => $this->input->post('desc'),
+					'detail' => $this->input->post('detail'),
+					'type' => $this->input->post('type'),
+					'venue' => $this->input->post('venue'),
+					'address' => $this->input->post('address'),
+					'logo' => $this->input->post('logo'),
+					'ticket_total' => $this->input->post('ticket_total')
+				);
+				try {
+					$this->db->insert('event', $data);
+					$event_id = $this->db->insert_id();
+					$status = "success";
+					$event = array('event_id' => $event_id);
+					echo json_encode(array('status' => $status, 'event' => $event));
+					return;
+				} catch (Exception $e) {
+					$status = "error";
+					$msg = "Database error.";
+				}
+			}	
 		}
 
 		echo json_encode(array('status' => $status, 'msg' => $msg));
