a7af42db3a5fbde7fa25b0adef11b936d56ac0b6
Leo Deng <myst.dg@gmail.com>
2012-08-31 16:15:01 +0800

    add several constants add authentication helper set auth_helper to autoload other assessories

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'encrypt','image_lib');
+$autoload['libraries'] = array('database', 'encrypt', 'image_lib');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database', 'encrypt','image_lib');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'form', 'file');
+$autoload['helper'] = array('url', 'form', 'file', 'auth');
 
 
 /*
---
+++ application/helpers/auth_helper.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	function client_auth() {
+        $CIevent =& get_instance();
+
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $args_array = explode('&', $_SERVER['QUERY_STRING']);
+        } else {
+            $args_array = array();
+            foreach($_POST as $pkey => $pvalue) {
+                array_push($args_array, $pkey . '=' . $pvalue);
+            }
+        }
+        sort($args_array, SORT_STRING);
+        $retrived_string = implode('&', $args_array);
+
+        return base64_encode($_SERVER['REQUEST_METHOD'] . ':' . rawurlencode($CIevent->uri->uri_string() . '?' . $retrived_string) . IEVENT_AUTHENTICATION) == $_SERVER['HTTP_IEVENT_SIGNATURE'];
+	}
